47ad22213ee08efa4b49a0ff0a09f2608c379597
GavMS <97010002+GavMS@users.noreply.github.com>
2025-12-31 20:51:49 +0700

    yes

--- Sistem Les Privat Coding (Laravel Experiment)/public/js/main.js
+++
@@ -1,9 +0,0 @@
-// Main JavaScript File
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Sistem Les Privat Coding - Loaded');
-
-    // Future: Add Sidebar Toggle Logic
-    const sidebar = document.querySelector('.sidebar');
-    // ...
-});
